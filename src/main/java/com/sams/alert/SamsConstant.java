0b610c2d50513b6cdf0795c92b8dc167ceb5a696
vn0yrph <vn0yrph@LGECX1901500282.homeoffice.Wal-Mart.com>
2019-11-08 13:43:10 +0530

    init

---
+++ src/main/java/com/sams/alert/SamsConstant.java
@@ -0,0 +1,10 @@
+package com.sams.alert;
+
+public interface SamsConstant {
+	
+	String CONNECTION="AccountEndpoint=https://dev-sams-smart-alerts-sql.documents.azure.com:443/;AccountKey=j0vtAyi43Sbds4i7kdWU2hviYAHQ7eoM38G3ZfvClLcQ6Wu6TugU0EzV0CQhwq93G6w4qjYMAsmaOEOWLdZYmQ==;";
+	String DATABASE="dev-sams-smart-alerts-sql";
+	String COLLECTION="items";
+		
+
+}
